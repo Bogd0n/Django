2d35f0fb0ff7b1c61efd46dcc0c5de2a74a274fe
IlyaVoltov <ivoltovp@gmail.com>
2025-03-10 18:38:00 +0300

    правильная версия

--- Example/main/views.py
+++ Example/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Hello, Django!")
+
+def about(request):
+    return HttpResponse("Это страница О нас!")
+
+def contact(request):
+    return HttpResponse("Контакты: email@example.com, +7 123 456 7890")
